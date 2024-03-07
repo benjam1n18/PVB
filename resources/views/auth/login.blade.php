810e5469ff6e68aac29948a42b27e452b6ba0471
benjam1n18 <benjamincaffa@gmail.com>
2024-03-07 16:04:22 +0100

    Week 1 en login style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center mt-5">
         <div class="col-md-8">
 
-            <div class="card">
+        <div class="paper_block">
                 <div class="card-header">Login</div>
                 <div class="card-body">
                     <form action="{{ route('authenticate') }}" method="post">
